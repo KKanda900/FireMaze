881cc9eeb78706872b4a4cf3b1eda971c1eb810b
Agam1410 <48292808+Agam1410@users.noreply.github.com>
2021-02-09 23:59:57 -0500

    sleep for each step

--- MazeGUI.py
+++ MazeGUI.py
@@ -1,7 +1,7 @@
 import pygame, sys, re, random, numpy, math
 from pygame_widgets import Button
 from collections import deque, OrderedDict
-import threading
+import threading, time
 
 # Color Graphics used in the Maze Visualizer
 BLACK = (0, 0, 0)
@@ -66,28 +66,29 @@ class MazeGUI:
         fire_array = self.fire_array
         fire_array_copy = fire_array
 
-        while bln:  # for the first one does a random fire
-            y = random.randint(0, len(fire_maze) - 1)
-            x = random.randint(0, len(fire_maze) - 1)
-            if fire_maze[x][y] != 2 and fire_maze[x][y] != 1:
-                fire_array[x][y] = 2
-                break
-
-        for i in range(0, len(self.tracking_obstacles) - 1):
-            for j in range(0, len(self.tracking_obstacles) - 1):
-                fire = 0
-                if fire_maze[i][j] != 1 and fire_array[i][j] != 2:
-                    if fire_array_copy[i+1][j] == 2:
-                        fire += 1
-                    if fire_array_copy[i-1][j] == 2 and i != 0:
-                        fire += 1
-                    if fire_array_copy[i][j+1] == 2:
-                        fire += 1
-                    if fire_array_copy[i][j-1] == 2 and j != 0:
-                        fire += 1
-                    prob = 1 - ((1 - q)**fire)
-                    if fire > 0 and random.random() <= prob:
-                        fire_array[i][j] = 2
+        if bln:
+            while bln:  # for the first one does a random fire
+                y = random.randint(0, len(fire_maze) - 1)
+                x = random.randint(0, len(fire_maze) - 1)
+                if fire_maze[x][y] != 2 and fire_maze[x][y] != 1:
+                    fire_array[x][y] = 2
+                    break
+        else:
+            for i in range(0, len(self.tracking_obstacles) - 1):
+                for j in range(0, len(self.tracking_obstacles) - 1):
+                    fire = 0
+                    if fire_maze[i][j] != 1 and fire_array[i][j] != 2:
+                        if fire_array_copy[i+1][j] == 2:
+                            fire += 1
+                        if fire_array_copy[i-1][j] == 2 and i != 0:
+                            fire += 1
+                        if fire_array_copy[i][j+1] == 2:
+                            fire += 1
+                        if fire_array_copy[i][j-1] == 2 and j != 0:
+                            fire += 1
+                        prob = 1 - ((1 - q)**fire)
+                        if fire > 0 and random.random() <= prob and prob > 0:
+                            fire_array[i][j] = 2
 
 
         print(fire_array)
@@ -275,6 +276,7 @@ def start():
             maze.generate_fire_maze(.1, False)
         else:
             maze.generate_fire_maze(.1, True)
+        time.sleep(1.5)
     running = True
     index = 0
     while running:
