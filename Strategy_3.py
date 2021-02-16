6783f1dd45f22e1ea00f875ff78adf6d3822cc11
Karun Kanda <kkanda900@gmail.com>
2021-02-15 23:49:04 -0500

    final draft of strategy_3

--- Strategy_3.py
+++ Strategy_3.py
@@ -10,11 +10,11 @@ BLUE = (0,0,255)
 RED = (255,0,0)
 
 class FireNode:
-    prob_fire = 0.0
+    fire_prob = 0.0
     value = 0
 
     def __init__(self, value, prob_fire):
-        self.prob_fire = prob_fire
+        self.fire_prob = prob_fire
         self.value = value
 
 class MazeGUI:
@@ -73,21 +73,23 @@ class MazeGUI:
     def generate_fire_maze(self, probability):
         q = probability
         fire_maze = self.tracking_obstacles
+        #print("Hello fire maze",fire_maze)
+        everything = numpy.zeros((len(fire_maze), len(fire_maze[0])))
         fire = 0
         fire_array = self.fire_array
         fire_array_copy = fire_array
-        fire_index = self.fire_index 
-
-        if fire_index == 0:
+        #print("hello fire array",self.fire_array)
+        if self.fire_index == 0:
             x = random.randint(0, len(fire_maze) - 1)
             y = random.randint(0, len(fire_maze) - 1)
             if fire_maze[x][y] != 2 and fire_maze[x][y] != 1:
                 fire_array[x][y] = 2
+                self.tracking_obstacles[x][y] = 2
                 self.fire_maze[x][y] = FireNode(2, 1.0) # represents the fire
-            fire_index += 1
+            self.fire_index += 1
         else:
-            for i in range(0, self.dim - 1):
-                for j in range(0, self.dim - 1):
+            for i in range(0, len(self.tracking_obstacles) - 1):
+                for j in range(0, len(self.tracking_obstacles) - 1):
                     fire = 0
                     if fire_maze[i][j] != 1 and fire_array[i][j] != 2:
                         if fire_array_copy[i+1][j] == 2:
@@ -101,9 +103,10 @@ class MazeGUI:
                         prob = 1 - ((1 - q)**fire)
                         if fire > 0 and random.random() <= prob and prob > 0:
                             fire_array[i][j] = 2
+                            self.tracking_obstacles[i][j] = 2
                             self.fire_maze[i][j] = FireNode(2, 1.0)
                     elif fire_maze[i][j] == 1:
-                        self.fire_maze[i][j] = FireNode(1, 0.0) # represents an obstacle
+                        self.fire_maze[i][j] = FireNode(1, 0.0)
                     else:
                         if fire_array_copy[i+1][j] == 2:
                             fire += 1
@@ -114,9 +117,7 @@ class MazeGUI:
                         if fire_array_copy[i][j-1] == 2 and j != 0:
                             fire += 1
                         prob = 1 - ((1 - q)**fire)
-                        self.fire_maze[i][j] = FireNode(0, prob) # represents the free cell with no obstacle or fire
-
-        self.fire_index = fire_index
+                        self.fire_maze[i][j] = FireNode(0, prob)
         self.x = 0
         self.y = 0
         screen = self.display
@@ -155,17 +156,15 @@ class MazeGUI:
     def check_valid_bounds(self, i, j, pop_value, arr):
         i = pop_value[0] + i
         j = pop_value[1] + j
-        if i >= 0 and i < len(arr) and j >= 0 and j < len(arr):
+        if i >= 0 and i < len(arr) and j >= 0 and j < len(arr) and arr[i][j] != 1:
             return True
         else:
             return False
     
     def fire_route_search(self, start): # implemented using a modified bfs
         arr = self.tracking_obstacles
-
         # now define the start and end node which in our case is the first indicies and the last indicies respectively
-        # also defined is a start for everytime you want to start the maze at a certain point ((0, 0) to start then all according to bfs)
-        goal = (len(arr) - 1, len(arr) - 1) # ending will stay consistent 
+        goal = (self.dim-1, self.dim-1)
 
         # now because we are working with bfs, we know bfs calls for a fringe in the form of a queue because of the queue's policy (FIFO)
         fringe = deque()
@@ -177,28 +176,11 @@ class MazeGUI:
         # for this implementation of bfs we want to keep track of the parents to obtain the shortest path
         path = []
 
-        if self.fire_maze[start[0]][start[1]].value == 2:
-            print("Start of maze on fire, can't proceed")
-            return []
-        elif self.fire_maze[goal[0]][goal[1]].value == 2:
-            print("Ending on fire can't proceed")
-            return []
-
         # now iterate through the fringe to check for the path
         while len(fringe) > 0:
-            
-            if self.fire_maze[goal[0]][goal[1]].value == 2:
-                print("Ending on fire can't proceed")
-                return []
-
+            #print(fringe)
             current = fringe.popleft()
-
-            if self.fire_maze[current[0]][current[1]] == 2:
-                print("OUCH IM ON FIRE")
-                return []
-
             visited[current[0]][current[1]] = True
-
             if current == goal:
                 path.append(current)
                 path.reverse()
@@ -223,34 +205,33 @@ class MazeGUI:
 
                 bfs_route.append(start)
                 bfs_route.reverse()
+                #print(bfs_route)
                 return bfs_route
 
             else:
                 # first check the up direction
-                if self.check_valid_bounds(-1, 0, current, arr) and self.fire_maze[current[0] - 1][current[1]].value == 0 and  self.fire_maze[current[0] - 1][current[1]].prob_fire < 0.3 and visited[current[0] - 1][current[1]] == False and (current[0] - 1, current[1]) not in fringe:
+                if self.check_valid_bounds(-1, 0, current, arr) and arr[current[0] - 1][current[1]] == 0 and self.fire_maze[current[0] - 1][current[1]].fire_prob < .3 and visited[current[0] - 1][current[1]] == False and (current[0] - 1, current[1]) not in fringe:
                     fringe.append((current[0] - 1, current[1]))
                     if current not in path:
                         path.append(current)
 
                 # now check the down direction
-                if self.check_valid_bounds(1, 0, current, arr) and self.fire_maze[current[0] + 1][current[1]].value == 0 and self.fire_maze[current[0] + 1][current[1]].prob_fire < 0.3 and visited[current[0] + 1][current[1]] == False and (current[0] + 1, current[1]) not in fringe:
+                if self.check_valid_bounds(1, 0, current, arr) and arr[current[0] + 1][current[1]] == 0 and self.fire_maze[current[0] + 1][current[1]].fire_prob < .3 and visited[current[0] + 1][current[1]] == False and (current[0] + 1, current[1]) not in fringe:
                     fringe.append((current[0] + 1, current[1]))
                     if current not in path:
                         path.append(current)
 
                 # now we can check the left direction
-                if self.check_valid_bounds(0, -1, current, arr) and self.fire_maze[current[0]][current[1] - 1].value == 0 and self.fire_maze[current[0]][current[1] - 1].prob_fire < 0.3 and visited[current[0]][current[1] - 1] == False and (current[0], current[1] - 1) not in fringe:
+                if self.check_valid_bounds(0, -1, current, arr) and arr[current[0]][current[1] - 1] == 0 and self.fire_maze[current[0]][current[1] - 1].fire_prob < .3 and visited[current[0]][current[1] - 1] == False and (current[0], current[1] - 1) not in fringe:
                     fringe.append((current[0], current[1] - 1))
                     if current not in path:
                         path.append(current)
 
                 # finally check the right side
-                if self.check_valid_bounds(0, 1, current, arr) and self.fire_maze[current[0]][current[1] + 1].value == 0 and self.fire_maze[current[0]][current[1] + 1].prob_fire < 0.3 and visited[current[0]][current[1] + 1] == False and (current[0], current[1] + 1) not in fringe:
+                if self.check_valid_bounds(0, 1, current, arr) and arr[current[0]][current[1] + 1] == 0 and self.fire_maze[current[0]][current[1] + 1].fire_prob < .3 and visited[current[0]][current[1] + 1] == False and (current[0], current[1] + 1) not in fringe:
                     fringe.append((current[0], current[1] + 1))
                     if current not in path:
                         path.append(current)
-        
-        print('No Route Found')
         return []
 
     def escape_the_fire(self):
@@ -258,14 +239,17 @@ class MazeGUI:
         DEAD = False
         start = (0, 0)
         while ALIVE:
-            self.generate_fire_maze(0.1)
+            self.generate_fire_maze(0.2)
+            time.sleep(1)
             escape_route = self.fire_route_search(start)
-            time.sleep(1.5)
-            if escape_route == []:
+            print(len(escape_route))
+            if len(escape_route) == 0:
                 DEAD = True
                 ALIVE = False
+                break
             elif escape_route[0] == (self.dim-1, self.dim-1):
                 break
+            print(escape_route)
             self.draw_path(escape_route[1])
             start = escape_route[1]
             
@@ -283,7 +267,7 @@ class MazeGUI:
         curr = None
         
         curr = position
-        tracking_array[curr[0]][curr[1]] = 2
+        tracking_array[curr[0]][curr[1]] = 3
                 
         for k in range(0, size):
             self.x = 20
@@ -304,11 +288,11 @@ class MazeGUI:
                 elif tracking_array[k][b] == 2:
                     cell = pygame.Rect(
                         self.x, self.y, self.cell_size, self.cell_size)
-                    pygame.draw.rect(screen, BLUE, cell)
-                elif tracking_array[k][b] == -1:
+                    pygame.draw.rect(screen, RED, cell)
+                elif tracking_array[k][b] == 3:
                     cell = pygame.Rect(
                         self.x, self.y, self.cell_size, self.cell_size)
-                    pygame.draw.rect(screen, RED, cell)
+                    pygame.draw.rect(screen, BLUE, cell)
                 else:
                     cell = pygame.Rect(
                         self.x, self.y, self.cell_size, self.cell_size)
@@ -316,7 +300,7 @@ class MazeGUI:
                 pygame.display.update()
                 self.x += 20
 
-def start():
+def strategy_3():
 
     # command line arguments
     dim = int(sys.argv[1])
@@ -349,4 +333,4 @@ def start():
         pygame.display.update()
 
 if __name__ == "__main__":
-    start()
+    strategy_3()
